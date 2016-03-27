a766929987942396ab636499fa5e1882f3d0b422
aj07mm <aj07mm@hotmail.com>
2016-03-27 16:47:14 -0300

    working

--- test/subtest_three/foo.py
+++ test/subtest_three/foo.py
@@ -1,2 +1,2 @@
 # -*- encoding: utf-8 -*-
-# -*- encoding: utf-8 -*-
+your_custom_escaped_content
--- unicode-all.sh
+++ unicode-all.sh
@@ -2,5 +2,7 @@ TEXT_LINE=$1
 
 for file in $(find ./test -type d -exec echo {}\/\*.py \;)
 do
-	echo $TEXT_LINE >> $file
+	echo $TEXT_LINE > temp_file.txt
+	cat $file >> temp_file.txt
+	mv temp_file.txt $file
 done
